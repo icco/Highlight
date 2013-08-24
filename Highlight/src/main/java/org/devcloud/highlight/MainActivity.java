bd6bae9d2132b5df3876bf36b21365026d1ddccc
Nat Welch <nat@natwelch.com>
2013-08-24 15:17:28 -0700

    Highlight

--- Highlight/src/main/java/org/devcloud/highlight/MainActivity.java
+++ Highlight/src/main/java/org/devcloud/highlight/MainActivity.java
@@ -3,21 +3,28 @@ package org.devcloud.highlight;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
 
+  /**
+   * Called when the user clicks the Send button
+   */
+  public void sendMessage(View view) {
+    // Do something in response to button
+  }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
+  }
 }
