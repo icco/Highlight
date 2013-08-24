d063b924f3fde00f0d9368ed58655e362a73a116
Nat Welch <nat@natwelch.com>
2013-08-24 15:31:35 -0700

    Dicking Around

--- Highlight/src/main/java/org/devcloud/highlight/MainActivity.java
+++ Highlight/src/main/java/org/devcloud/highlight/MainActivity.java
@@ -1,12 +1,16 @@
 package org.devcloud.highlight;
 
-import android.os.Bundle;
+import android.R;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends Activity {
+
+  public final static String EXTRA_MESSAGE = "org.devcloud.highlight.MESSAGE";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +22,10 @@ public class MainActivity extends Activity {
    * Called when the user clicks the Send button
    */
   public void sendMessage(View view) {
-    // Do something in response to button
-  }
+    Intent intent = new Intent(this, DisplayMessageActivity.class);
+    EditText editText = (EditText) findViewById(R.id.edit_message);
+    String message = editText.getText().toString();
+    intent.putExtra(EXTRA_MESSAGE, message);
 
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.main, menu);
-    return true;
   }
 }
