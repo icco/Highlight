32da079b68be934f43ff48893fd2b46142fbceb2
Nat Welch <nat@natwelch.com>
2013-08-24 17:09:28 -0700

    workin it

--- Highlight/src/main/java/org/devcloud/highlight/HighlightMessage.java
+++ Highlight/src/main/java/org/devcloud/highlight/HighlightMessage.java
@@ -1,7 +1,32 @@
 package org.devcloud.highlight;
 
+import java.util.Date;
+
 /**
- * Created by nat on 8/24/13.
+ * Class for storing messages.
  */
 public class HighlightMessage {
+  String text;
+  Date created;
+
+  public HighlightMessage(String msg) {
+    this.setText(msg);
+    this.setCreated(new Date());
+  }
+
+  public String getText() {
+    return text;
+  }
+
+  public Date getCreated() {
+    return created;
+  }
+
+  private void setText(String text) {
+    this.text = text;
+  }
+
+  private void setCreated(Date created) {
+    this.created = created;
+  }
 }
