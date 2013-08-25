10ff038065d757844bd998beec4ccfca7c55172a
Nat Welch <nat@natwelch.com>
2013-08-24 17:30:34 -0700

    thoughts

--- Highlight/src/main/java/org/devcloud/highlight/HighlightMessage.java
+++ Highlight/src/main/java/org/devcloud/highlight/HighlightMessage.java
@@ -1,12 +1,23 @@
 package org.devcloud.highlight;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
 import java.util.Date;
 
 /**
  * Class for storing messages.
  */
 public class HighlightMessage {
+  private final String COLUMN_NAME_TEXT = "text";
+  private final String COLUMN_TYPE_TEXT = "text";
+
   String text;
+
+  private final String COLUMN_NAME_DATE = "created";
+  private final String COLUMN_TYPE_DATE = "datetime";
+
   Date created;
 
   public HighlightMessage(String msg) {
@@ -28,5 +39,23 @@ public class HighlightMessage {
 
   private void setCreated(Date created) {
     this.created = created;
+  }
+
+  public void save(Context context) {
+    DbHelper mDbHelper = new DbHelper(context);
+// Gets the data repository in write mode
+    SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+// Create a new map of values, where column names are the keys
+    ContentValues values = new ContentValues();
+    values.put(this.COLUMN_NAME_TEXT, this.getText());
+    values.put(this.COLUMN_NAME_DATE, this.getCreated());
+
+// Insert the new row, returning the primary key value of the new row
+    long newRowId;
+    newRowId = db.insert(
+        this.TABLE_NAME,
+        this.COLUMN_NAME_NULLABLE,
+        values);
   }
 }
